88e452789b854c031f2b964150cc4381ecc7ae4e
MB-study <MB-study@192.168.1.118>
2020-08-10 12:13:04 +0800

    Java

---
+++ src/main/java/com/pcschool/ocb/test/BMI.java
@@ -0,0 +1,17 @@
+
+package com.pcschool.ocb.test;
+
+import java.util.Scanner;
+
+public class BMI {
+    public static void main(String[] args) {
+        System.out.println("Hello BMI");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("請輸入身高:");
+        double h = sc.nextDouble();
+        System.out.println("請輸入體重:");
+        double w = sc.nextDouble();
+        double bmi = w / Math.pow(h/100, 2);
+        System.err.printf("身高: %.1f 體重: %.1f BMI: %.2f\n", h, w, bmi);
+    }
+}
