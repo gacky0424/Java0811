e759dbbaddc37b95cd5d14d266a44484c8bc00f9
MB-study <MB-study@192.168.1.118>
2020-08-11 10:11:42 +0800

    Java

--- src/main/java/com/pcschool/ocb/d01/Dog.java
+++ src/main/java/com/pcschool/ocb/d01/Dog.java
@@ -2,7 +2,7 @@
 package com.pcschool.ocb.d01;
 
 public class Dog {
-    public String type; //品種
+    public static String type; //品種,+static->共用
     public int age;     //年齡
     public int price;   //價格
     public int amount;  //數量
