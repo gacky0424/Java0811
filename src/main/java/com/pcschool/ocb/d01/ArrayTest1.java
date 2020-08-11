cdd51dbab8cf23a56343db432879d41fc814d747
MB-study <MB-study@192.168.1.118>
2020-08-11 13:35:24 +0800

    Java

---
+++ src/main/java/com/pcschool/ocb/d01/ArrayTest1.java
@@ -0,0 +1,13 @@
+package com.pcschool.ocb.d01;
+
+public class ArrayTest1 {
+    public static void main(String[] args) {
+        int[] scores = {100, 90, 80, 70};
+        System.out.println(scores[0]);
+        System.out.println(scores[1]);
+        System.out.println(scores[2]);
+        System.out.println(scores[3]);
+        System.out.println(scores.length);
+
+    }
+}
--- src/main/java/com/pcschool/ocb/d01/ReferenceTest.java
+++ src/main/java/com/pcschool/ocb/d01/ReferenceTest.java
@@ -6,7 +6,17 @@ public class ReferenceTest {
     public static void main(String[] args) {
         String s1 = new String("Java");
         String s2 = s1;
-        System.out.println("s1");
-        System.out.println("s2");
+        System.out.println(s1);
+        System.out.println(s2);
+        
+        String s3 = "Hello";
+        append(s3);
+    
     }
+    public static void append(String s) {//相當於 s = s3
+        s = s + "Java";
+        System.out.println(s);
+            
+    }
+    
 }
