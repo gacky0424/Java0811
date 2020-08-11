1179e73928dd43900584e8a2f21d22c1b97754cd
MB-study <MB-study@192.168.1.118>
2020-08-11 14:50:37 +0800

    Java

---
+++ src/main/java/com/pcschool/ocb/d01/ArrayTest2.java
@@ -0,0 +1,27 @@
+
+package com.pcschool.ocb.d01;
+
+public class ArrayTest2 {  
+    public static void main(String[] args) {
+        //股票獲利
+        double[] stocks = {7.6, 3.9, 15.6, 28.3, 1.2, 10.8, 35.3, 45.6, 10.2, 0.5};
+        //債劵獲利
+        double[] bounds = {6.8, 7.2, 6.8, 7.5, 6.9, 7.9, 7.9, 7.1, 7.2};
+        double sum1=0;
+        for(int a = 0; a<stocks.length;a++){
+            sum1 += stocks[a];
+        }
+        double avg1 = sum1/(double)stocks.length;
+        System.out.printf("股票平均獲利: %.1f\n", avg1);
+        
+        
+        int sum2=0;
+        for(int a = 0; a<bounds.length;a++){
+            sum2 += bounds[a];
+        }
+        double avg2 = sum2/(double)bounds.length;
+        System.out.printf("債券平均獲利: %.1f\n", avg2);
+
+    }
+    
+}
