027f1dfff207e12b31653fc50348e79a3da8fa9a
MB-study <MB-study@192.168.1.118>
2020-08-11 11:39:59 +0800

    Java

---
+++ src/main/java/com/pcschool/ocb/d01/Account.java
@@ -0,0 +1,22 @@
+package com.pcschool.ocb.d01;
+
+public class Account {
+    
+    private int balance;
+    //存款
+    public void save(int money){
+        if(money > 0){
+            balance += money;//balance = balance + money
+        }
+    }
+    //提款
+    public void withdraw(int money){
+        if(money > 0 && balance >= 0){
+            balance -= money;//balance = balance - money
+        }
+    }
+    //取得帳戶餘額
+    public int getBalance(){
+        return balance;
+    } 
+}
