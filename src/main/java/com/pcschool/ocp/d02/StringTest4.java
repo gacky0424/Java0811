0629122be2dc9c57da85ab6d4200354f4acb9a02
MB-study <MB-study@192.168.1.118>
2020-08-12 13:19:54 +0800

    Java

--- src/main/java/com/pcschool/ocp/d02/StringTest4.java
+++ src/main/java/com/pcschool/ocp/d02/StringTest4.java
@@ -2,17 +2,32 @@ package com.pcschool.ocp.d02;
 
 public class StringTest4 {
     public static void main(String[] args) {
-           String s ="Java";
+           String s = "Mr";
+           StringBuilder sb = new StringBuilder("Mr");
+           System.out.println(s);
+           System.out.println(sb);
+           updateString(s);
+           updateStringBuilder(sb);
+           System.out.println(s);
+           System.out.println(sb);
+           /*String s ="Java";
            StringBuilder sb = new StringBuilder("Java");
            System.out.println(s);
            System.out.println(sb);
+           updateString(s);
+           updateStringBuilder(sb);
+           System.out.println(s);
+           System.out.println(sb);*/
+           
     }
     
     public static void updateString(String s) {
-        s = s.concat("8");
+        s = s.concat(".Smith");
+        /*s = s.concat("8");*/
     }
     
     public static void updateStringBuilder(StringBuilder sb) {
-        sb.append("8");
+        sb.append(".Smith");
+        /*sb.append("8");*/
     }
 }
