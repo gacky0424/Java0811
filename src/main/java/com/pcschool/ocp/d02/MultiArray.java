858d3c386d235dc40cd66edabafc4f117b9c6457
MB-study <MB-study@192.168.1.118>
2020-08-12 13:22:13 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d02/MultiArray.java
@@ -0,0 +1,9 @@
+package com.pcschool.ocp.d02;
+
+public class MultiArray {
+    public static void main(String[] args) {
+        int[][] x = new int[2][3];
+        System.out.println(x);
+        
+    }
+}
