a42162c03fdafcfcf3393ab91a671204d37c1b68
MB-study <MB-study@192.168.1.118>
2020-08-12 13:33:50 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d02/MultiArray2.java
@@ -0,0 +1,11 @@
+package com.pcschool.ocp.d02;
+
+public class MultiArray2 {
+    public static void main(String[] args) {
+        int[][] x = /*new int[][]*/ {{100, 90,80},{70, 60 ,50}};
+        // 將 x[0] 的內容取出
+        for(int i=0;i<x[0].length;i++){
+            System.out.println(x[0][i]);
+        }
+    }
+}
