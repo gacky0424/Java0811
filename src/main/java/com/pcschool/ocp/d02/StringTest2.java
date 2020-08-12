38ed9dff0f13e21525eeaffbb9650d0a71e36753
MB-study <MB-study@192.168.1.118>
2020-08-12 11:33:54 +0800

    Java

--- src/main/java/com/pcschool/ocp/d02/StringTest2.java
+++ src/main/java/com/pcschool/ocp/d02/StringTest2.java
@@ -4,9 +4,12 @@ public class StringTest2 {
     public static void main(String[] args) {
         String s1 = "Java";
         String s2 = "Java";
+        String s3 = new String("Java");//s3 在 pool 外面,不可共用
         System.out.println(s1 == s2);
         System.out.println(s1.equals(s2));
-
-
+        System.out.println(s1 == s3);
+        s3 = s3.intern();//利用 intern 把 s3 放到 pool 裡面,節省記憶體,可共用
+        System.out.println(s1 == s3);
+       
     }
 }
