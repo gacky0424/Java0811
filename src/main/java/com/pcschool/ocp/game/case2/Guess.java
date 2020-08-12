a4e7e3b23de91f1e9ae29eb9f470bfe72052df0c
MB-study <MB-study@192.168.1.118>
2020-08-12 15:52:20 +0800

    Java

--- src/main/java/com/pcschool/ocp/game/case2/Guess.java
+++ src/main/java/com/pcschool/ocp/game/case2/Guess.java
@@ -5,10 +5,10 @@ public class Guess {
         int[] ans = {3, 8, 4, 5};
         int[] user = {0, 0, 0, 0};
         //第一次猜
-        String userAns = "1234";
+        String userAns = "3485";
         //資料填入 user 陣列
         for(int i=0;i<user.length;i++){
-            user[0] = Integer.parseInt(userAns.charAt(0) + "");
+            user[i] = Integer.parseInt(userAns.charAt(i) + "");
         }
         //列出 user 陣列資料
         for(int x : user){
@@ -16,5 +16,24 @@ public class Guess {
         }
         System.out.println();
         //幾 A 幾 B ?
+        int a = 0;
+        int b = 0;
+        //計算 B
+        for(int ur : user){
+            for(int as : ans){
+                if(ur == as){
+                    b++;
+                }
+            }
+        }
+        //計算 A
+        for(int i=0;i<user.length;i++){
+            if(user[i] == ans[i]){
+                a++;
+            }
+        }
+        //減去多算的 B (同位置的)
+        b = b - a;
+        System.out.printf("%dA%dB\n",a ,b);
     }
 }
