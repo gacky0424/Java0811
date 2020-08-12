53767d152f37510a5fc4a5a5fba548aaef40d889
MB-study <MB-study@192.168.1.118>
2020-08-12 14:28:38 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d02/MultiArray5.java
@@ -0,0 +1,23 @@
+package com.pcschool.ocp.d02;
+
+public class MultiArray5 {
+
+    public static void main(String[] args) {
+        char[][] ttt = {
+                        {'O', 'O', 'X'},
+                        {'X', 'O', ' '},
+                        {'X', ' ', 'X'}
+                       };
+        int x = 7;
+        
+        ttt[x/3][x%1] = 'O';
+        System.out.println("O 勝利");
+        //印出最後結局
+        for(int i=0;i<ttt.length;i++){
+            System.out.println(ttt[i]);
+            /*for(int j=0;i<ttt[i].length;j++){
+                System.out.println(ttt[i][j]);
+            }*/
+        }
+    }
+}
