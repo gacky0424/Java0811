74119fbd0680f4bd8f5d351414f6b41e80b52af3
MB-study <MB-study@192.168.1.118>
2020-08-13 12:47:34 +0800

    Java

--- src/main/java/com/pcschool/ocp/d03/fastfood/FoodStore.java
+++ src/main/java/com/pcschool/ocp/d03/fastfood/FoodStore.java
@@ -5,7 +5,7 @@ public class FoodStore {
         Combine a = new Combine(4, 2, 3, 4);
         Combine b = new Combine(7, 3, 5, 6);
         Combine c = new Combine(1, 1, 1, 1);
-        Combine d = new Combine(9, 0, 0, 1);
+        Combine d = new Combine(300, 300, 300, 100);
         System.out.println(a.getPrice());
         System.out.println(b.getPrice());
         System.out.println(c.getPrice());
