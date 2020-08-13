6c4adc6d56dfe0bb6c386b3d18343114baf355a3
MB-study <MB-study@192.168.1.118>
2020-08-13 15:20:36 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d03/ArithmeticMethod2.java
@@ -0,0 +1,17 @@
+package com.pcschool.ocp.d03;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArithmeticMethod2 {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int[]scores = new int[10];
+        for(int i=0;i<scores.length;i++){
+            scores[i] = r.nextInt(100);
+        }
+        //Java8
+        Arrays.stream(scores).forEach(s -> System.out.print(s + ""));
+        System.err.println("");
+    }
+}
