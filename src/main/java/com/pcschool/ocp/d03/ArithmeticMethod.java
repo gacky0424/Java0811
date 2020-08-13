d72ded43fbd117c0703341e63232e23d79e2b2ef
MB-study <MB-study@192.168.1.118>
2020-08-13 09:26:21 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d03/ArithmeticMethod.java
@@ -0,0 +1,13 @@
+package com.pcschool.ocp.d03;
+
+public class ArithmeticMethod {
+    public static void main(String[] args) {
+        char[] poker = "A2345678910JQK".toCharArray();
+        for(char p : poker){
+            System.out.println(p + "");
+        }
+        System.out.println();
+        //shuffle 洗牌
+        
+    }
+}
