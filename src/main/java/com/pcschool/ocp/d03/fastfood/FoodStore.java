9d59dc8569b954e34cb698b9e0770e0d0086984f
MB-study <MB-study@192.168.1.118>
2020-08-13 13:33:56 +0800

    Java

--- src/main/java/com/pcschool/ocp/d03/fastfood/FoodStore.java
+++ src/main/java/com/pcschool/ocp/d03/fastfood/FoodStore.java
@@ -5,10 +5,12 @@ public class FoodStore {
         Combine a = new Combine(4, 2, 3, 4);
         Combine b = new Combine(7, 3, 5, 6);
         Combine c = new Combine(1, 1, 1, 1);
-        Combine d = new Combine(300, 300, 300, 100);
+        Combine d = new Combine(10, 10, 10, 10);
         System.out.println(a.getPrice());
         System.out.println(b.getPrice());
         System.out.println(c.getPrice());
         System.out.println(d.getPrice());
+        System.out.printf("庫存-> 雞腿: %d,雞胸: %d,雞翅: %d,可樂: %d\n",
+                    Leg.getAmount(),Breast.getAmount(),Wings.getAmount(),Coca.getAmount());
     }
 }
--- src/main/java/com/pcschool/ocp/d03/fastfood/Leg.java
+++ src/main/java/com/pcschool/ocp/d03/fastfood/Leg.java
@@ -3,7 +3,14 @@ package com.pcschool.ocp.d03.fastfood;
 
 public class Leg {
     private int price = 45;
+    private static int amount = 100;
+    public Leg(){
+        amount--;
+    }
     public int getPrice() {
         return price;
+    }
+    public static int getAmount() {
+        return amount;
     }
 }
