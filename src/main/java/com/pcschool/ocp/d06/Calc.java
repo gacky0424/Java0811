62798ec73b55bc76b1166ef9473f37c1662efd53
MB-study <MB-study@192.168.1.118>
2020-08-17 12:12:02 +0800

    Java

--- src/main/java/com/pcschool/ocp/d06/Calc.java
+++ src/main/java/com/pcschool/ocp/d06/Calc.java
@@ -3,7 +3,9 @@ package com.pcschool.ocp.d06;
 import java.util.Arrays;
 
 public class Calc {
-    public int add(int [] values){
+    public int add(int...values){
+        int sum = 0;
         return Arrays.stream(values).sum();
+
     } 
 }
