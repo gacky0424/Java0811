bd2749c0cbcc4e2730c0d054ea3e0edb091e9623
MB-study <MB-study@192.168.1.118>
2020-08-17 11:15:05 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d06/case2/Company2.java
@@ -0,0 +1,47 @@
+package com.pcschool.ocp.d06.case2;
+
+import com.pcschool.ocp.d06.case2.employee.Account;
+import com.pcschool.ocp.d06.case2.employee.Employee;
+import com.pcschool.ocp.d06.case2.employee.Manager;
+import com.pcschool.ocp.d06.case2.employee.Operator;
+import com.pcschool.ocp.d06.case2.employee.Sales;
+import com.pcschool.ocp.d06.case2.employee.Supervistor;
+
+public class Company2 {
+    public static void main(String[] args) {
+        Employee[] employees = {
+            new Supervistor(),
+            new Manager(),
+            new Manager(),
+            new Account(),
+            new Account(),
+            new Account(),
+            new Sales(),
+            new Sales(),
+            new Sales(),
+            new Sales(),
+            new Operator(),
+            new Operator(),
+            new Operator(),
+            new Operator(),
+            new Operator(),
+        };
+        // 資料分析
+        // 總薪資成本 = ?
+        int sum = 0;
+        for(Employee emp : employees) {
+            sum += emp.getSalary().getMoney();
+        }
+        System.out.printf("總薪資成本: %,d\n", sum);
+        //sales總薪資成本 = ?
+        sum = 0;
+        for(Employee emp : employees) {
+            if(emp instanceof Sales){
+                sum += emp.getSalary().getMoney();
+            }
+        }
+        System.out.printf("sales 總薪資成本: %,d\n", sum);
+        
+                
+    }
+}
