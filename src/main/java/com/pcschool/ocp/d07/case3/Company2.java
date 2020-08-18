b040a319222ba85931c2f820db6c509ba1913e1b
MB-study <MB-study@192.168.1.118>
2020-08-18 15:06:58 +0800

    Java

--- src/main/java/com/pcschool/ocp/d07/case3/Company2.java
+++ src/main/java/com/pcschool/ocp/d07/case3/Company2.java
@@ -4,7 +4,10 @@ public class Company2 {
     public static void main(String[] args) {
         Employee emp1 = new Employee();
         Employee emp2 = new Boss();
-        System.out.println(emp1.Salary);//直接抓屬性會從emp2前判斷,抓方法會從new後面判斷
+        System.out.println(emp1.Salary);//直接抓屬性會從(emp1前面)類別名判斷
+        emp1.job();//抓方法會從new後面的實例(Employee)去判斷
+        
         System.out.println(emp2.Salary);
+        emp2.job();
     }
 }
--- src/main/java/com/pcschool/ocp/d07/case3/Employee.java
+++ src/main/java/com/pcschool/ocp/d07/case3/Employee.java
@@ -1,11 +1,11 @@
 package com.pcschool.ocp.d07.case3;
 
 public class Employee {
-    public int Salary = 30000;
-    public void printSalary(){
+    public int Salary = 30000;//屬性
+    public void printSalary(){//方法
         System.out.println("支取薪資");
     }
-    public void job(){
+    public void job(){//方法
         System.out.println("寫程式");
     }
 }
