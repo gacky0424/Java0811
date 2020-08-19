88daf6c4db7c63e8f05a97b18a3d17c666d59806
MB-study <MB-study@192.168.1.118>
2020-08-19 10:20:34 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d08/case1/Chihuahua.java
@@ -0,0 +1,14 @@
+package com.pcschool.ocp.d08.case1;
+
+//吉娃娃
+public class Chihuahua implements Pet{
+
+    @Override
+    public void eat() {
+        System.out.println("寶路");
+    }
+    @Override
+    public void skill() {
+        System.out.println("坐下");
+    }
+}
