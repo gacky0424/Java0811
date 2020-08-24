e2d4fad15264856237fce63a445831d4176cd1a3
MB-study <MB-study@192.168.1.118>
2020-08-24 10:54:05 +0800

    Java

---
+++ src/main/java/com/pcschool/ocp/d11/MapDemo2.java
@@ -0,0 +1,51 @@
+package com.pcschool.ocp.d11;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class MapDemo2 {
+
+    public static void main(String[] args) {
+        Map<String, Integer> exams = new LinkedHashMap<>();
+        exams.put("國文", 100);
+        exams.put("英文", 100);
+        exams.put("數學", 90);
+        System.out.println(exams + "\n");
+        //資料走訪元素1
+        System.out.println(exams.get("國文"));
+        System.out.println(exams.get("英文"));
+        System.out.println(exams.get("數學") + "\n");
+        //資料走訪元素2
+        Set<String> keys = exams.keySet();
+        for (String key : keys) {
+            System.out.println(key + ":" + exams.get(key));
+        }
+        System.out.println();
+        //走訪元素3
+        Set<Entry<String, Integer>> entrySet = exams.entrySet();
+        for (Entry<String, Integer> entry : entrySet) {
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+        System.out.println();
+        //走訪元素4(可以利用 iterator.remobe() 來移除元素)
+        Iterator<Map.Entry<String, Integer>> iterator = exams.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Entry<String, Integer> entry = iterator.next();
+            System.out.println(entry.getKey() + ":" + entry.getValue());
+        }
+        System.out.println();
+        //走訪元素5(Java 8)
+        exams.forEach((key, value) -> System.out.println(key + ":" + value));
+        System.out.println();
+        //走訪元素6(Java 8 stream api)
+        exams.entrySet().stream().forEach(entry -> System.out.println(entry.getKey() + ":" + entry.getValue()));
+        System.out.println();
+
+    }
+}
---
+++ src/main/java/com/pcschool/ocp/d11/MapDemo4.java
@@ -0,0 +1,22 @@
+package com.pcschool.ocp.d11;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+public class MapDemo4 {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("John", "Helen", "Mary", "Happy", "Jo", "Vincent");
+        System.out.println(
+                names.stream()
+                //在方法類別(?)才能使用(輸入Import,能省略Collectors.)
+                .collect(groupingBy(String::length,counting()))
+        );
+        Map<Integer, Long> group1 = names.stream().collect(groupingBy(String::length, counting()));
+        System.out.println(group1);
+    
+    }
+}
